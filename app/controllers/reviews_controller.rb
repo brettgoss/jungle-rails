059c6df4be6d6bf13b55175046663816c8791671
Brett Goss <brettgoss@live.com>
2016-11-12 15:12:14 -0800

    Check if user is logged in before submitting review.
    If the user tries to submit a review and they aren't logged in, it will
    display an error.
    
    Also various ux/ui improvements with regards to displaying reviews.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,12 @@
 class ReviewsController < ApplicationController
+  before_action :require_login
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.new(review_params)
     @review.user_id = current_user.id
     if @review.save
       redirect_to @product
-    else
-      puts @review.error
     end
 
   end
@@ -18,4 +18,10 @@ private
     params.require(:review).permit(:rating, :description)
   end
 
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to review this product"
+      redirect_to Product.find(params[:product_id])
+    end
+  end
 end
