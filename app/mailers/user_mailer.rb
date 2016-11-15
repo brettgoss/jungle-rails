1d711daaa61703dda60cfb6ac16acc76a52b174c
Brett Goss <brettgoss@live.com>
2016-11-15 12:00:57 -0800

    reconfigure user mailer to work in production.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'no-reply@jungle.com'
+  default from: 'no-reply@fast-citadel-60478.herokuapp.com'
 
   def email_receipt(order)
     @order = order
-    @url  = 'localhost:3000'
+    @url  = 'https://fast-citadel-60478.herokuapp.com'
     mail(to: order.email, subject: "Order confirmation for order ##{@order.id}")
   end
 end
