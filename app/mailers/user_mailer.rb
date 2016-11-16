43b0cf9cc81cc63c90e287918dc6eafa4cef1fd5
Brett Goss <brettgoss@live.com>
2016-11-15 17:44:44 -0800

    Improve heroku error handling

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,6 +9,7 @@ class UserMailer < ApplicationMailer
       mail(to: order.email, subject: "Order confirmation for order ##{@order.id}")
     rescue Net::SMTPFatalError
       puts "User email isn't verified by MailGun"
+      redirect_to @order
     rescue
       puts "There was an error."
     end
